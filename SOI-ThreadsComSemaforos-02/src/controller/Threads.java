b8b8c0575e0ae45dfb51b726890faeda92ac2789
Rodrigo Martinez <rodrigomrtz@gmail.com>
2020-09-28 13:15:21 -0300

    Exercício 2

--- SOI-ThreadsComSemaforos-02/src/controller/Threads.java
+++ SOI-ThreadsComSemaforos-02/src/controller/Threads.java
@@ -46,22 +46,10 @@ public class Threads extends Thread{
 		System.out.println((prato +1) + "º prato iniciado: " + pratoId);
 		
 		
-		for (float i = 0; i <= tempo; i+=20){ 
-			
-/* 	For com passo de 20 em 20 para agilizar a demonstração do número, pois com o passo a 1 (i++),
- * 	o programaa teria mais linhas a serem impressas para indicar o percentual do 0% até o 100%.
- * 
- * 	O tempo de cada prato é fixo (pelo valor do Random) e não é alterado. O for de 20 em 20
- * 	agiliza a demonstração do %. 
- * 	
- * 	Eu entendi que não há a preocupação com o % e sim com a entrega, portanto, 
- * 	tanto faz se o % será detalhado acada 1%, 10% ou 20%.
- * 
- *   O 20 foi escolhido porque eu gostei da evolução de % impressa na tela.  
- */			 
+		for (double i = 0; i <= tempo; i+=100){ // laço do for para incremento de 0,1s (100 milissegundos) 
 			try {
 				sleep(100); // envio da info a cada 0,1 seg (100 milissegundos)
-				int percentual = (int) ((i / tempo) * 100);
+				int percentual = (int) ((i / tempo) * 100); // para transfomrar o número em XX %
 				System.out.println((prato +1) +"º prato - conluído em: " +percentual +" % ");
 			} catch (Exception erro) {				
 			}
@@ -73,7 +61,7 @@ public class Threads extends Thread{
 	private void entregarPrato() {
 		try {
 			sleep(500);
-			System.out.println((prato +1) +"º prato - " +pratoId +" : Entregue");
+			System.out.println((prato +1) +"º prato - " +pratoId +": Entregue");
 		} catch (Exception erro) {			
 		}
 	}
